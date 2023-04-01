16b217b461f5bb0d916ec8248aaf6cada9f0e2d3
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-04-01 14:42:48 -0300

    ex03

---
+++ estruturaCondicional/ex03/script.js
@@ -0,0 +1,7 @@
+const num1 = prompt('Digite um número');
+const num2 = prompt('Digite outro número');
+const num3 = prompt('Digite mais um número');
+
+const maior = Math.max(num1, num2, num3);
+
+document.write('Maior: ' + maior);
