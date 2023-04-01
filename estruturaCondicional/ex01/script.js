b271804779c1c1964315b4b9746113b48b7fe9f8
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-04-01 14:33:19 -0300

    ex01

---
+++ estruturaCondicional/ex01/script.js
@@ -0,0 +1,13 @@
+const letra = prompt("Digite uma letra:");
+
+if (
+  letra === "a" ||
+  letra === "e" ||
+  letra === "i" ||
+  letra === "o" ||
+  letra === "u"
+) {
+  alert("Vogal");
+} else {
+  alert("Consoante");
+}
