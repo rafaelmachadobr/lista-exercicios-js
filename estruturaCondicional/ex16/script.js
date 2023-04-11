cb0dd25aac634171e72a858b61c2ddea741aff79
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-04-11 17:13:23 -0300

    ex16

---
+++ estruturaCondicional/ex16/script.js
@@ -0,0 +1,7 @@
+const numero = parseInt(prompt("Digite um número: "));
+
+if (numero % 2 === 0) {
+  alert("O número " + numero + " é par");
+} else {
+  alert("O número " + numero + " é ímpar");
+}
